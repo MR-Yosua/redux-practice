4edff2adfa12541e836c7e388951c9dc8115e496
Josue Lavado <j.lavadoortubia@gmail.com>
2021-05-05 12:42:30 -0400

    pseudo auth finished

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ import RegisterScreen from './components/login/RegisterScreen';
 import { useDispatch, useSelector } from 'react-redux';
 import { auth } from './firebase';
 import { setActiveUser } from './features/userSlice';
-
+import './App.css';
 
 function App() {
   
   
   const dispatch = useDispatch();
   const [isLogged, setIsLogged] = useState(false);
+  const [checking, setChecking] = useState(true);
 
   const authenticaded = useSelector(state=>state.user.userEmail);
 
@@ -34,9 +35,23 @@ function App() {
           setIsLogged(false);
         }
       }
-    })
-  }, [dispatch,isLogged,authenticaded])
 
+      setChecking(false);
+    })
+  }, [dispatch,isLogged,authenticaded,setChecking])
+
+
+  if(checking){
+    return(
+        <div className="loader-main">
+          <div className="title-container">
+            <h1>Loading ...</h1>
+          </div>
+          <div className="loader__preloader">
+          </div>
+        </div>
+    )
+  }
 
   return (
     <Router>
